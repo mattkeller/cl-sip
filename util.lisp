155d325808502327970536e02d27681cd65ee0f1
Matt Keller <mk@littleredbat.net>
2008-10-09 00:17:18 -0400

    Added aif,awhen,etc to utils

--- util.lisp
+++ util.lisp
@@ -5,3 +5,20 @@
   "Give an alist of '((symbol . symbol-name) ...)"
   (mapcar (lambda (s) (cons s (symbol-name s))) symbols))
 
+(defmacro if-bind (var test &body then/else)
+  (assert (first then/else)
+          (then/else)
+          "IF-BIND missing THEN clause.")
+  (destructuring-bind (then &optional else)
+      then/else
+    `(let ((,var ,test))
+       (if ,var ,then ,else))))
+
+(defmacro aif (test then &optional else)
+  `(if-bind it ,test ,then ,else))
+
+(defmacro when-bind (var test &body body)
+  `(if-bind ,var ,test (progn ,@body)))
+
+(defmacro awhen (test &body body)
+  `(when-bind it ,test ,@body))
