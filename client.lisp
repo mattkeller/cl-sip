0dbc791d7dac55ea20e6d1ec10ff0e45e2e362ce
Matt Keller <mk@littleredbat.net>
2009-11-11 08:15:44 -0500

    Rough sketches of client code

--- client.lisp
+++ client.lisp
@@ -20,16 +20,121 @@
 
 (in-package :cl-sip.client)
 
-(defun make-client (&key username proxy passwd)
-  "Build a new SIP client"
+(defparameter sip-default-port 5060)
+
+(defvar uac-port sip-default-port
+  "Local port to listen for incoming SIP messages on")
+
+(defparameter default-max-forwards 70)
+
+(defvar branch-counter 0
+  "Incr and add this to branch-prefix to get a globally unique branch id")
+
+(defparameter branch-prefix "z9hG4bK")
+
+(defclass user-proxy ()
+  ((host  :initarg  :host ;; dns or ip?
+          :initform (error "Must have a host")
+          :reader   host)
+   (port  :initarg  :port
+          :initform 5060
+          :reader   port)
+   (proto :initform 'udp)))
+
+(defclass subscriber ()
+  ((user-name :initarg  :user-name
+              :initform (error "Must have a user-name")
+              :reader   user-name)
+   (disp-name :initarg :disp-name
+              :initform "Anonymous"
+              :reader   disp-name)
+   (proxy     :initarg :proxy
+              :initform (error "Must have a user-proxy")
+              :accessor proxy)))
+
+(defparameter client-us-states '(offline busy online))
+
+(defclass client-ua ()
+  ((subscriber :initarg  :subscriber
+               :initform (error "Must have a subscriber")
+               :reader   subscriber)
+   (local-port :initarg  :local-port
+               :initform sip-default-port
+               :reader   local-port)
+   (local-ip   :initarg  :local-ip
+               :initform nil
+               :reader   local-ip) ;; for contact header
+   sessions ;; list of sessions
+   state    ;; online, offline, busy, etc
+   ))
+
+(defclass session () ;; TODO
+  (call-id
+   to
+   from
+   dialogs))
+
+(defclass dialog () ;; TODO
+  (id
+   local-seq-num
+   remote-seq-num
+   local-uri
+   remote-uri
+   remote-target
+   secure-p
+   route-set
+   early-p))
+
+(defun new-request (method to &optional from)
+  (unless from
+    (setq from (concatenate'string "\"" user-display-name "\" <" user-name "@" domain ">")))
+  ;; generate call-id, cseq, via, max-forwards
+  ;; add contact if invite
+  ;; case on method....
   nil)
 
-(defun call (client userb)
-  "Call userb, return a new session"
-  nil)
+(defun new-call-id ()
+  "Generate a globally unique call-id"
+  "nil")
 
-(defun answer (session)
-  "Attempt to answer the new call"
-  nil)
-  
+(defvar example-com-proxy (make-instance 'user-proxy :host "example.com"))
 
+(defvar bob-sub (make-instance 'subscriber
+                               :user-name "bob"
+                               :disp-name "Bob Jones"
+                               :proxy example-com-proxy))
+
+(defvar bob-ua (make-instance 'client-ua
+                              :subscriber bob-sub
+                              :local-port 5060))
+
+;; Totally broken, but you get the idea
+(defun  find-dialog (msg dialogs)
+  "Match the incoming message to a dialog or session"
+  (labels ((dialog-id (m)
+             `((:call-id ,(call-id m)) (:remote-tag ,(to-tag m)) (:local-tag ,(from-tag m))))
+           (dialog-id-eql (m1 m2)
+             (let ((id1 (dialog-id m1))
+                   (id2 (dialog-id m2)))
+               (and (string-equal (car (assoc :call-id id1)) (car (assoc :call-id id2)))
+                    (string-equal (car (assoc :remote-tag id1)) (car (assoc :local-tag id2)))
+                    (string-equal (car (assoc :local-tag id1)) (car (assoc :remote-tag id2)))))))
+    (find msg dialogs :test #'dialog-id-eql)))
+#|
+(socket-connect skt (make-inet-address "127.0.0.1") 5060)
+(socket-send skt "foo" 3)
+
+(host-ent-address (get-host-by-name "localhost")) => #(127 0 0 1)
+|#
+
+(defun make-sip-socket (addr &optional (port sip-default-port))
+  (let ((skt (make-instance 'inet-socket :type :datagram :protocol :udp)))
+    (socket-connect skt (make-inet-address addr) port)
+    skt))
+
+(defun send-msg (socket msg-string)
+  (socket-send socket msg-string (length msg-string)))
+    
+(defun lookup-address (host)
+  (handler-case (host-ent-address (get-host-by-name host))
+    (name-service-error (e) nil)))
